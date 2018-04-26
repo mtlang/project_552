747bcd6ccf58088e63990e7461fc81736fc6c758
cooknj <ncook3@wisc.edu>
2018-04-26 16:02:27 -0500

    multi-byte writes & correct read
    added read enable signal and tag/data array specific write enables. Also added logic for shifting the word enable for multi-byte writes.

--- Stage 3/cache_tb.v
+++ Stage 3/cache_tb.v
@@ -1,17 +1,22 @@
 module cache_tb ();
-reg clk, rst, Write_Enable;
+reg clk, rst, Read_Enable, Write_Data_Array, Write_Tag_Array;
+reg [2:0] Word_Num;
 reg [15:0] Address, Data_In;
 wire [15:0] Data_Out;
 wire Miss;
 integer i;
 
-cache DUT (.clk(clk), .rst(rst), .Address(Address), .Data_In(Data_In), .Data_Out(Data_Out), .Write_Enable(Write_Enable), .Miss(Miss));
+cache DUT (.clk(clk), .rst(rst), .Address(Address), .Data_In(Data_In), .Data_Out(Data_Out), .Write_Data_Array(Write_Data_Array), 
+				.Write_Tag_Array(Write_Tag_Array), .Read_Enable(Read_Enable), .Miss(Miss), .Word_Num(Word_Num));
 
 initial begin
 i=0;
 clk = 0;
 rst = 1;
-Write_Enable = 0;
+Read_Enable = 0;
+Write_Data_Array = 0;
+Write_Tag_Array = 0;
+Word_Num = 3'h0;
 Address = 16'h0000;
 Data_In = 16'h0000;
 #10;
@@ -20,42 +25,48 @@ rst = 0;
 #10;
 
 //Write stuff to cache
-Address = 16'h1811;
-for(i=0; i<10; i=i+1) begin
+Address = 16'h1810;
+for(i=0; i<8; i=i+1) begin
 	Data_In = Data_In + 1;
-	Address = Address + 1;
-	Write_Enable = 1;
+	Write_Tag_Array = (i > 0) ? 0 : 1;
+	Write_Data_Array = 1;
+	Word_Num = (i > 0) ? Word_Num + 1 : 3'h0;
 	#10;
+	
 end
 
-Address = 16'h1901; 
+Data_In = 16'h00ff;
+Address = 16'h1900; 
 for(i=0; i<10; i=i+1) begin
 	Data_In = Data_In +1;
-	Address = Address + 1;
-	Write_Enable = 1;
+	Write_Tag_Array = (i > 0) ? 0 : 1;
+	Write_Data_Array = 1;
+	Word_Num = (i > 0) ? Word_Num + 1 : 3'h0;
 	#10;
 end
 
 //Read stuff from cache
-Address = 16'h1811;
-for(i=0; i<10; i=i+1) begin
-	Address = Address + 1;
-	Write_Enable = 0;
+Write_Data_Array = 0;
+Address = 16'h1810;
+for(i=0; i<8; i=i+1) begin
+	Address = (i > 0) ? Address + 1 : Address;
+	Read_Enable = 1;
 	#10;
 end
 
-Address = 16'h1901; 
+Address = 16'h1900; 
 for(i=0; i<10; i=i+1) begin
-	Address = Address + 1;
-	Write_Enable = 0;
+	Address = (i > 0) ? Address + 1 : Address;
+	Read_Enable = 1;
 	#10;
 end
+/*
 Address = 16'h1001; //Only one of these reads should result in a non-Miss
 for(i=0; i<20; i=i+1) begin
 	Address = Address + 129;
 	Write_Enable = 0;
 	#10;
-end
+end */
 #10;
 
 $stop();
